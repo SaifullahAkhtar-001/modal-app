84c1216287a2e6ee62879b94768ca7ce41cfa294
Saifullah Akhtar <118631470+SaifullahAkhtar-001@users.noreply.github.com>
2023-08-30 22:17:45 +0500

    Update Modal.vue

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,12 +1,10 @@
 <template>
     <div @click.self="closeModel" class="fixed top-0 w-full h-full bg-blue-500 bg-opacity-80">
-        <div class="w-[400px] p-8  mt-96 mx-auto bg-blue-700 rounded-xl text-white">
-            <div style="padding: auto;">
+        <div class="w-[400px] p-8 my-32 mx-auto bg-blue-700 rounded-xl text-white">
                 <slot />
                 <div class="mt-4">
                     <slot name="links"></slot>
                 </div>
-            </div>
         </div>
     </div>
 </template>
