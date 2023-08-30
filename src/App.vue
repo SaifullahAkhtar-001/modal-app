3fbc9b8b7c0c8057cd71e0f1e838343b92d9b0f7
Saifullah Akhtar <118631470+SaifullahAkhtar-001@users.noreply.github.com>
2023-08-30 22:12:26 +0500

    make modal trigger

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,44 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <p class="text-6xl font-bold underline underline-offset-8 mb-8">
+    {{ title }}
+  </p>
+  <p class="text-gray-500 mb-2">Welcome</p>
+  <Teleport to="#Modal" v-if="showModal">
+    <Modal :header="header" :text="text" @close="toggleModal">
+      <p class="text-6xl font-bold">{{ header }}</p>
+      <p class="text-2xl">{{ text }}</p>
+      <template v-slot:links>
+        <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 ml-4">subscribe</a>
+        <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 ml-4 ">unsubscribe</a>
+      </template>
+    </Modal>
+  </Teleport>
+  <button @click="toggleModal" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Show
+    Modal</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Modal
+  },
+  data() {
+    return {
+      header: 'Hi, Saifullah here!',
+      text: 'please Subscribe me',
+      title: 'Hello World',
+      showModal: false,
+    }
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal
+    }
   }
+
 }
 </script>
 
---
+++ src/components/Modal.vue
@@ -0,0 +1,23 @@
+<template>
+    <div @click.self="closeModel" class="fixed top-0 w-full h-full bg-blue-500 bg-opacity-80">
+        <div class="w-[400px] p-8  mt-96 mx-auto bg-blue-700 rounded-xl text-white">
+            <div style="padding: auto;">
+                <slot />
+                <div class="mt-4">
+                    <slot name="links"></slot>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    props: ['header', 'text'],
+    methods: {
+        closeModel() {
+            this.$emit('close')
+        }
+    }
+}
+</script>
+<style></style>
